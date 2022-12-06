e4064d7d04cb81c17713ae4a908f9906d23d7630
El3asfora <Fareseslam777@gmail.com>
2022-12-06 11:36:25 +0200

    E-learning

---
+++ index.js
@@ -0,0 +1,46 @@
+// Express & DotEnv & Cors & Port \\
+
+const express = require('express')
+
+const app = express()
+
+const dotenv = require('dotenv')
+
+const cors = require('cors')
+
+dotenv.config()
+
+const HostName = process.env.HOSTNAME
+
+const Port = process.env.Port
+
+app.use(cors())
+
+app.use(express.urlencoded({ extended: false }))
+
+app.use(express.json())
+
+app.use(express.static('public'))
+
+app.set('views', './views')
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req , res) => {
+    res.render('Index')
+})
+
+// Router Middleware \\
+
+
+// Redirect Error 404 File \\
+
+app.use((req , res) => {
+    res.status(404).render('404')
+})
+
+// Listen Port  \\
+
+app.listen(Port,HostName, ()=>{
+    console.log(`You Run On: http://${HostName}:${Port}/`)
+})
